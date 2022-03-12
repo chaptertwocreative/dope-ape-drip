4c08b99d38891b439cad842d31a490d57137ddf6
it <it@gmail.com>
2022-03-12 23:34:23 +0530

    Updated code

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,43 +1,45 @@
-export const NetworkContextName = "Kovan Test Network";
-export const ACTIVE_NETWORK = 42;
-export const contractAddress = "0xB494DB91929ca1FcA9b740446B45Aa2B79403987";
+// Testing CONFIG
 
-export const RPC_URL =
-  "https://kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
-
-export const NetworkDetails = [
-  {
-    chainId: "0x2A",
-    chainName: "Kovan Test Network",
-    nativeCurrency: {
-      name: "Kovan Test Network",
-      symbol: "ETH",
-      decimals: 18,
-    },
-    rpcUrls: [RPC_URL],
-    blockExplorerUrls: ["https://kovan.etherscan.io"],
-  },
-];
-
-// MAINNET CONFIG
-
-// export const NetworkContextName = "Ethereum Mainnet";
-// export const ACTIVE_NETWORK = 1;
-// export const contractAddress = "MAINNET_CONTRACT_ADDRESS";
+// export const NetworkContextName = "Kovan Test Network";
+// export const ACTIVE_NETWORK = 42;
+// export const contractAddress = "0xB494DB91929ca1FcA9b740446B45Aa2B79403987";
 
 // export const RPC_URL =
-//   "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
+//   "https://kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
 
 // export const NetworkDetails = [
 //   {
-//     chainId: "0x1",
-//     chainName: "Ethereum Mainnet",
+//     chainId: "0x2A",
+//     chainName: "Kovan Test Network",
 //     nativeCurrency: {
-//       name: "Ethereum Mainnet",
+//       name: "Kovan Test Network",
 //       symbol: "ETH",
 //       decimals: 18,
 //     },
 //     rpcUrls: [RPC_URL],
-//     blockExplorerUrls: ["https://etherscan.io"],
+//     blockExplorerUrls: ["https://kovan.etherscan.io"],
 //   },
 // ];
+
+// MAINNET CONFIG
+
+export const NetworkContextName = "Ethereum Mainnet";
+export const ACTIVE_NETWORK = 1;
+export const contractAddress = "0xb5d5ba5a8cb511a2344d94ef432472f8b7773d7c";
+
+export const RPC_URL =
+  "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
+
+export const NetworkDetails = [
+  {
+    chainId: "0x1",
+    chainName: "Ethereum Mainnet",
+    nativeCurrency: {
+      name: "Ethereum Mainnet",
+      symbol: "ETH",
+      decimals: 18,
+    },
+    rpcUrls: [RPC_URL],
+    blockExplorerUrls: ["https://etherscan.io"],
+  },
+];
